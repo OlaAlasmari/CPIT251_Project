bd1e111533a56945a6b0c3331e7bb6e41bfc00e1
Hanin <haneenaldosari3@gmail.com>
2024-02-13 16:53:45 +0300

    First Commit!

--- src/cpit251_project/CPIT251_Project.java
+++ src/cpit251_project/CPIT251_Project.java
@@ -7,6 +7,7 @@ public class CPIT251_Project {
     
     public static void main(String[] args) {
         System.out.println("Ola Alasmari");
+        System.out.println("Hanin Aldosary");
         
     }
     
