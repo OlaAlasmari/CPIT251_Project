94ea25a1b353212fd0e6719350ed7c85c93a3709
Ola <olaalioa123@gmail.com>
2024-04-19 18:53:49 +0300

    CPIT251 Project

--- src/cpit251_project/Supervisor.java
+++ src/cpit251_project/Supervisor.java
@@ -1,14 +1,22 @@
 
 package cpit251_project;
 
+import java.util.ArrayList;
+
 
 public class Supervisor extends User{
 
     Place place;
     int sID;
+    ArrayList<Supervisor> supervisors = new ArrayList<>();
+    ArrayList<Place> Places = new ArrayList<>();
 
     public Supervisor(Place place, int sID) {
         this.place = place;
+        this.sID = sID;
+    }
+    
+    public Supervisor(int sID) {
         this.sID = sID;
     }
 
@@ -46,6 +54,25 @@ public class Supervisor extends User{
 
     public boolean deleteEvaluation(Place place) {
         return false;
+    }
+    
+    public void AddSupervisor(){
+        supervisors.add(new Supervisor(sID));
+        System.out.println("\nSupervisor Added Sucessfuly");
+    }
+    
+    public static boolean isSupervisorIdPresent(ArrayList<Supervisor> supervisors, int sID) {
+        for (Supervisor supervisor : supervisors) {
+            if (supervisor.getsID() == sID) {
+                return true; // Return true if the ID is found
+            }
+        }
+        return false; // Return false if the ID is not found
+    }
+    
+    public void AddPlace(Place place){
+        Places.add(place);
+        System.out.println("\nPlace Added Sucessfuly");
     }
 
 }
